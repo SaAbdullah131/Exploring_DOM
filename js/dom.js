ebd4692578da2bb3db913d44baa8b172e550ed91
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-02-09 12:09:21 +0600

    Style Manipulation using js DOM

--- js/dom.js
+++ js/dom.js
@@ -1,2 +1,14 @@
-console.log("Hello from js");
-console.log(document.body);
+const sections  = document.querySelectorAll("section");
+for(const section of sections) {
+    section.style.border = "2px solid steelblue";
+    section.style.marginBottom = "5px";
+    section.style.borderRadius = "15px";
+    section.style.paddingLeft = "7px";
+    section.style.backgroundColor = "gray";
+}
+// const placeContainer = document.getElementById("places-container");
+// placeContainer.style.backgroundColor = "yellow";
+
+const placeContainer = document.getElementById("places-container");
+placeContainer.classList.add("text-center");
+placeContainer.classList.remove("large-text");
